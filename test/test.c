b25d1f043d5e819ed222069ca3751a7c6a3e2129
momoya-N <momoyalikephysics@gmail.com>
2024-12-25 21:30:31 +0900

    修論執筆中

--- test/test.c
+++ test/test.c
@@ -1,25 +1,6 @@
 #include <stdio.h>
 
-void sum(int a,int b, int *c);
-
-int main(void){
-    int a=1,b=2;
-    int d=0;
-    int *c=&d;
-    
-    // c=&d;
-    // *c=10;
-    printf("c = %p\n",c);
-    printf("c = %d\n",*c);
-    sum(a,b,c);
-    printf("c = %d\n",*c);
-    printf("%d + %d = %d\n",a,b,*c);
-
+int main() {
+    printf("Hello, New World!\n");
     return 0;
-}
-
-void sum(int a,int b, int *c){
-    printf("c = %d\n",*c);
-    *c = a+b;
-    printf("c = %d\n",*c);
 }
---
+++ test/test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    string b;
+
+    cin >> a;
+    b = (string)a;
+
+    cout << b[0] << endl;
+    return 0;
+}
